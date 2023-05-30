18d6ff7887647b2261e89b31b9c6a133b6b17ef3
Baiganjia <134911905+Baiganjia@users.noreply.github.com>
2023-05-30 10:45:43 +0800

    加入TMDB热门影片，支持输出没有的影片到csv

--- EMBY_HotMovie_Importer.py
+++ EMBY_HotMovie_Importer.py
@@ -1,30 +1,33 @@
-import requests
-import json
+import os,requests,json,time,base64,csv
 from bs4 import BeautifulSoup
-import time
-import base64
 import urllib.parse
 
-
-EMBY_SERVER = 'https://XXXXX'  # 这里填入你自己的EMBY服务器
-API_KEY = 'XXXX'  # 这里填入你自己的EMBY API密钥
-TMDB_API_KEY = 'XXXXX' # 这里填入你自己的TMDB API密钥
-
-COLLECTION_ID = "2719225"
-COLLECTION_NAME = "【豆瓣TMDB热门电影】"
+# 这里设置代理
+# os.environ['http_proxy'] = 'http://127.0.0.1:7890'
+# os.environ['https_proxy'] = 'http://127.0.0.1:7890'
 
 class Get_Detail(object):
+
     def __init__(self):
+        # 这里填入你Emby服务器地址
+        self.emby_server = 'https://BAIGAN.EMBY.SERVER' 
+        # 这里填入你Emby API密钥
+        self.emby_api_key = 'XXXXXXXXEMBYXXXXXXXXXXXXXXXX' 
+        # 这里填入你自己的TMDB API密钥
+        self.tmdb_api_key = 'XXXXXXXXTMDBXXXXXXXXXXX' 
+        # 设置要添加电影到指定合集的ID，不指定则按下面指定的名称自动创建
+        self.collection_id = "2333333" 
+        # 设置要创建的合集的名称
+        self.collection_name = "【豆瓣和TMDB热门电影】" 
+        # 这里设置你要获取的豆瓣电影数量
+        self.movies_per_page = 20  
+        self.noexit = []
         self.ctls = []
-        self.emby_server = EMBY_SERVER
-        self.api_key = API_KEY
-        self.collection_id = COLLECTION_ID
-        self.collection_name = COLLECTION_NAME
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36 Edg/101.0.1210.39"
         }
-        self.start_url = "https://movie.douban.com/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&sort=recommend&page_limit=20&page_start={}"
-        self.urllist = [self.start_url.format(i*20) for i in range(1)] 
+        self.start_url = f"https://movie.douban.com/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&sort=recommend&page_limit={self.movies_per_page}&page_start={{}}"
+        self.urllist = [self.start_url.format(i*self.movies_per_page) for i in range(1)] 
         
 
     def get_imdb_id(self, url):
@@ -39,7 +42,7 @@ class Get_Detail(object):
         return imdb_id
 
     def search_emby_by_imdb_id(self, imdb_id):
-        url = f"{self.emby_server}/emby/Items?api_key={self.api_key}&Recursive=true&IncludeItemTypes=Movie&AnyProviderIdEquals=imdb.{imdb_id}"
+        url = f"{self.emby_server}/emby/Items?api_key={self.emby_api_key}&Recursive=true&IncludeItemTypes=Movie&AnyProviderIdEquals=imdb.{imdb_id}"
         response = requests.get(url)
         data = response.json()
         if response.status_code == 200 and data.get('TotalRecordCount', 0) > 0:
@@ -49,7 +52,6 @@ class Get_Detail(object):
 
     def create_collection(self):
         collection_name = self.collection_name
-
         first_movie_imdb_id = self.ctls[0]["imdb_id"]
         emby_data = self.search_emby_by_imdb_id(first_movie_imdb_id)
         if emby_data:
@@ -59,7 +61,7 @@ class Get_Detail(object):
             return None
 
         encoded_collection_name = urllib.parse.quote(collection_name, safe='')
-        url = f"{self.emby_server}/emby/Collections?IsLocked=false&Name={encoded_collection_name}&Ids={emby_id}&api_key={self.api_key}"
+        url = f"{self.emby_server}/emby/Collections?IsLocked=false&Name={encoded_collection_name}&Ids={emby_id}&api_key={self.emby_api_key}"
         headers = {
             "accept": "application/json"
         }
@@ -74,9 +76,11 @@ class Get_Detail(object):
 
     def get_value(self, urllist):
         cont_dict_list = []
+        print("开始获取豆瓣电影，避免防爬虫设置了每2秒请求间隔，因此会比较慢。。。")
+        total_movies = len(urllist) * self.movies_per_page  # 使用类的成员变量
+        current_movie = 0
         for each in urllist:
             response = requests.get(each, headers=self.headers)
-
             dictionary = json.loads(response.text)
             for eachdict in dictionary["subjects"]:
                 content = {}
@@ -87,10 +91,13 @@ class Get_Detail(object):
                 content["imdb_id"] = self.get_imdb_id(eachdict["url"])
                 cont_dict_list.append(content)
                 time.sleep(2)
+                current_movie += 1
+                print(f"\r进度: {current_movie}/{total_movies}", end='')
+        print()
         return cont_dict_list
 
     def add_movie_to_collection(self, emby_id, collection_id):
-        url = f"{self.emby_server}/emby/Collections/{collection_id}/Items?Ids={emby_id}&api_key={self.api_key}"
+        url = f"{self.emby_server}/emby/Collections/{collection_id}/Items?Ids={emby_id}&api_key={self.emby_api_key}"
         headers = {"accept": "*/*"}
         response = requests.post(url, headers=headers)
         response.raise_for_status()
@@ -101,7 +108,7 @@ class Get_Detail(object):
         image_content = response.content
         base64_image = base64.b64encode(image_content).decode('utf-8')
 
-        url = f'{self.emby_server}/emby/Items/{library_id}/Images/Primary?api_key={self.api_key}'
+        url = f'{self.emby_server}/emby/Items/{library_id}/Images/Primary?api_key={self.emby_api_key}'
 
         headers = {
             'Content-Type': 'image/jpeg'
@@ -110,35 +117,34 @@ class Get_Detail(object):
         response = requests.post(url, headers=headers, data=base64_image)
 
         if response.status_code == 204:
-            print(f'成功替换合集封面 {library_id}.')
+            print(f'成功更新合集封面 {library_id}.')
         else:
-            print(f'合集封面替换失败 {library_id}.')
+            print(f'合集封面更新失败 {library_id}.')
 
     def check_collection_exists(self, collection_id):
-        url = f"{self.emby_server}/emby/Items/{collection_id}?api_key={self.api_key}"
+        url = f"{self.emby_server}/Items?Ids={collection_id}&api_key={self.emby_api_key}"
         response = requests.get(url)
-        if response.status_code != 200:
-            print(f"Error: Received status code {response.status_code} when checking collection {collection_id}.")
-            return False
-        data = response.json()
-        if data.get('Type') != 'BoxSet':
-            print(f"Error: Item {collection_id} exists but it is not a collection.")
-            return False
-        print(f"Collection {collection_id} exists.")
-        return True
+        if response.status_code == 200:
+            data = response.json()
+            if len(data["Items"]) > 0 and data["Items"][0]["Type"] == "BoxSet":
+                return True
+        return False
     
 
     def get_tmdb_popular_movies(self, api_key):
+        print("开始获取TMDB热门电影...")
         base_url = "https://api.themoviedb.org/3"
         headers = {
             "Accept": "application/json",
         }
-        response = requests.get(f"{base_url}/movie/popular?api_key={api_key}", headers=headers)
+        # 注意这里添加了language=zh-CN参数
+        response = requests.get(f"{base_url}/movie/popular?api_key={api_key}&language=zh-CN", headers=headers)
         if response.status_code == 200:
             return response.json()['results']
         else:
             print(f"An error occurred: {response.status_code}")
             return []
+
 
     def get_movie_details(self, movie_id, api_key):
         base_url = "https://api.themoviedb.org/3"
@@ -149,24 +155,25 @@ class Get_Detail(object):
         if response.status_code == 200:
             return response.json()
         else:
-            print(f"An error occurred: {response.status_code}")
+            # print(f"An error occurred: {response.status_code}")
             return None
 
     def run(self):
         # 先获取豆瓣和TMDB的电影信息
+        
         self.ctls = self.get_value(self.urllist)
-        tmdb_movies = self.get_tmdb_popular_movies(TMDB_API_KEY)
+        tmdb_movies = self.get_tmdb_popular_movies(self.tmdb_api_key)
 
         # 将TMDB热门电影列表转化为与豆瓣电影同样的格式
         for movie in tmdb_movies:
-            details = self.get_movie_details(movie['id'], TMDB_API_KEY)
+            details = self.get_movie_details(movie['id'], self.tmdb_api_key)
             self.ctls.append({
                 '影名': movie['title'],
                 'imdb_id': details['imdb_id'] if details else None,
             })
 
         collection_id = self.collection_id
-        print(collection_id)
+
 
         if not self.check_collection_exists(collection_id):
             print('指定合集不存在合集，开始重新创建合集')
@@ -185,7 +192,7 @@ class Get_Detail(object):
             emby_data = self.search_emby_by_imdb_id(imdb_id)
             if emby_data:
                 emby_id = emby_data["Items"][0]["Id"]
-                image_url = f"{self.emby_server}/emby/Items/{emby_id}/Images/Primary?api_key={self.api_key}"
+                image_url = f"{self.emby_server}/emby/Items/{emby_id}/Images/Primary?api_key={self.emby_api_key}"
                 response = requests.head(image_url)
                 if response.status_code == 200:
                     break
@@ -199,20 +206,27 @@ class Get_Detail(object):
                     emby_id = emby_data["Items"][0]["Id"]
                     added_to_collection = self.add_movie_to_collection(emby_id, collection_id)
                     if added_to_collection:
-                        print(f"电影 {emby_id} 加入到合集 {collection_id} 内成功.")
+                        print(f"电影 '{movie['影名']}' 加入到合集成功.")
                     else:
-                        print(f"电影 {emby_id} 加入到合集 {collection_id} 内失败.")
-                    print("电影:", movie["影名"])
-                    print("IMDb ID:", imdb_id)
-                    print("Emby ID:", emby_id)
-                    print()
+                        print(f"电影 '{movie['影名']}' 加入到合集内失败.")
+                    # print("电影:", movie["影名"])
+                    # print("IMDb ID:", imdb_id)
+                    # print("Emby ID:", emby_id)
                 else:
-                    print(f"Emby内没有匹配到该IMDbID的电影: {imdb_id}")
-                    print()
+                    print(f"当前Emby库内还没有 '{movie['影名']}' {imdb_id}")
+                    message = [movie['影名'], imdb_id]
+                    self.noexit.append(message)
+            self.write_to_csv()  # 如果不想输出CSV 就注释掉这一行
         else:
             print("该电影似乎缺失海报.")
 
-
+            
+    def write_to_csv(self):
+        with open('noexit.csv', 'w', newline='', encoding='utf-8-sig') as f:
+            writer = csv.writer(f)
+            writer.writerow(['电影名', 'imdbID'])  # writing the header
+            writer.writerows(self.noexit)  # writing the data
+                
 if __name__ == "__main__":
     gd = Get_Detail()
     gd.run()
